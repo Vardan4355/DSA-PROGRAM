4b9f356895647a18b95015e6b23a7b93fe867c7e
Vardan4355 <sharmavardan257@gmail.com>
2025-02-28 15:23:24 +0530

    Update Expression_evaluation.c

--- Expression_evaluation.c
+++ Expression_evaluation.c
@@ -86,3 +86,185 @@ int result = evalInfixExpression(str);
 printf("Result = d\n", result);
     return 0;
 }
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+int top1 = -1, top2 = -1;
+int operand[100];
+char operator[100];
+int precedence(char opr)
+{
+    switch (opr)
+    {
+    case '+':
+    case '-':
+        return 1; // lowest precedence.
+    case '*':
+    case '/':
+    return 2 ; // higher than +-.
+    case '^':
+        return 3; // higher than */.
+    default:
+    return 0;
+}
+}
+void doprocess()
+{
+    int x = operand[top1--];
+    int y = operand[top2--];
+    char opr *operator[top2--];
+    int z;
+    switch (opr)
+    {
+    case '+':
+        z = y + x;
+        break;
+    case '*':
+        z = y * x;
+        break;
+    case '-':
+        z = y - x;
+        ;
+        break;
+    case '/':
+        z = y / x;
+        ;
+        break;
+    case '^':
+        z = pow(y, x);
+        break;
+    }
+    operand[++top1] = z;
+}
+int evalInfixExpression(char *exp)
+{
+    int size = strlen(exp);
+    for (int i = 0; i < size; i++)
+    {
+        char ch = exp[i];
+        if (isdigit(ch))
+        {
+            int num = 0;
+            while(isdigit(exp[i])){
+                num = num*10 + exp[i]-'0';
+                i++;
+            }
+            i--;
+            operand[++top1] = ch - '0'; // 0 or 48/
+        }
+        else if (ch == '+' || ch == '*' || ch == '/' || ch == '^')
+        {
+        }
+        else if (ch == '(')
+        {
+            operand[++top2] = ch;
+        }
+        else if (ch == ')')
+        {
+        }
+    }
+}
+int evalPostfixExpression(char *exp)
+{
+    int size = strlen(exp);
+    for(int i=0; i<size; i++)
+    {
+        char ch = exp[i];
+        if(isdigit(ch))
+        {
+            int num =0; 
+            while(isdigit(exp[i]))
+            {
+                num = num*10 + exp[i]-'0';
+                i++;
+            }
+            i--;
+            operand[++top1]=num;
+        }
+    }
+     else if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^'){
+            int x = operand[top1--];
+            int y = operator[top1--];
+            int z;
+            switch(ch){
+     case '+':
+        z = y + x;
+        break;
+    case '*':
+        z = y * x;
+        break;
+    case '-':
+        z = y - x;
+        ;
+        break;
+    case '/':
+        z = y / x;
+        ;
+        break;
+    case '^':
+        z = pow(y, x);
+        break;
+    }
+    operand[++top1] = z;
+}
+ }
+ int evalPrefixExpression(char *exp)
+{
+    int size = strlen(exp);
+    for(int i=size-1; i>=0; i--)
+    {
+        char ch = exp[i];
+        if(isdigit(ch))
+        {
+            int num =0, place =1; 
+            while(isdigit(exp[i]))
+            {
+                num = num = num  + (exp[i]-'0')*place;
+                i--;
+                place = palce*10;
+            }
+            i++;
+            operand[++top1]=;
+        }
+    }
+     else if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^'){
+            int x = operand[top1--];
+            int y = operator[top1--];
+            int z;
+            switch(ch){
+     case '+':
+        z = y + x;
+        break;
+    case '*':
+        z = y * x;
+        break;
+    case '-':
+        z = y - x;
+        ;
+        break;
+    case '/':
+        z = y / x;
+        ;
+        break;
+    case '^':
+        z = pow(y, x);
+        break;
+    }
+    operand[++top1] = z;
+}
+}
+int main()
+{
+char str[100];
+printf("Enter infix expression :");
+gets(str);
+int result = evalInfixExpression(str);
+printf("Result = d\n", result);
+char str[100];
+printf("Enter postfix expression");
+gets(str);
+int result = evalPostfixExpression(str);
+printf("Result = d\n", result);
+    return 0;
+} 
